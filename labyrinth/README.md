dc7892c41dba4fcff1307c78928fed80e7446205
UnMoutonPerdu <mtnhugo062@gmail.com>
2025-01-13 10:01:26 -0500

    Labyrinth

---
+++ labyrinth/README.md
@@ -0,0 +1,16 @@
+# Labyrinth - Genuary 2025 #1
+
+## Horizontal and Vertical lines
+![labyrinth-horiz](./labyrinth-horiz.png)
+![labyrinth-horiz](./labyrinth-horiz-7.png)
+![labyrinth-horiz](./labyrinth-horiz-7.png)
+
+The following was unexpected due to an error in the way I coded the lines:
+
+![labyrinth-horiz](./sick-labyrinth.png)
+
+## Diagonal lines
+![labyrinth-diag](./labyrinth-diag-5.png)
+![labyrinth-diag](./labyrinth-diag-6.png)
+![labyrinth-diag](./labyrinth-diag-7.png)
+![labyrinth-diag](./labyrinth-diag-8.png)
---
+++ labyrinth/sketch.js
@@ -0,0 +1,54 @@
+let shape = 1; // 0 for horizontal and vertical lines - 1 for diagonal lines
+let maxDepth = 6;
+
+function setup() {
+  createCanvas(600, 600);
+  background(220);
+  strokeWeight(1.5);
+}
+
+function draw() {
+  if (shape == 0) {
+    recursiveDraw(20, 20, width-40, height-40, maxDepth);
+  }
+  else {
+    recursiveDrawDiag(20, 20, width-40, height-40, maxDepth);
+  }
+  noLoop();
+}
+
+function recursiveDraw(x, y, w, h, depth) {
+  if (depth == 0) {
+    if (random() > 0.5) {
+      line(x+w/2, y, x+w/2, y+h);
+    }
+    else {
+      line(x, y+h/2, x+w, y+h/2);
+    }
+  }
+
+  else {
+    recursiveDraw(x, y, w/2, h/2, depth-1);
+    recursiveDraw(x+w/2, y, w/2, h/2, depth-1);
+    recursiveDraw(x, y+h/2, w/2, h/2, depth-1);
+    recursiveDraw(x+w/2, y+h/2, w/2, h/2, depth-1);
+  }
+}
+
+function recursiveDrawDiag(x, y, w, h, depth) {
+  if (depth == 0) {
+    if (random() > 0.5) {
+      line(x, y, x+w, y+h);
+    }
+    else {
+      line(x+w, y, x, y+h);
+    }
+  }
+
+  else {
+    recursiveDrawDiag(x, y, w/2, h/2, depth-1);
+    recursiveDrawDiag(x+w/2, y, w/2, h/2, depth-1);
+    recursiveDrawDiag(x, y+h/2, w/2, h/2, depth-1);
+    recursiveDrawDiag(x+w/2, y+h/2, w/2, h/2, depth-1);
+  }
+}
