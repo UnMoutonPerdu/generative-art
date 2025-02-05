3de106853643c4811d5f714da7a932c400092006
Hugo <mtnhugo062@gmail.com>
2025-02-05 11:05:41 -0500

    trees

--- trees/sketch2.js
+++ trees/sketch2.js
@@ -1,20 +1,23 @@
 // Canvas Configuration
-let WIDTH = 800;
-let HEIGHT = 600;
+let WIDTH = 1400;
+let HEIGHT = 800;
 
 let lSystem;
-let numberRecursions = 8;
+let numberRecursions = 7;
 let sequence;
 let x = 0;
 let y = 0;
 
-let len = 200;
-let scale = 0.5;
-let angle = 35;
-let lineWeight = 5;
-
-let numberTrees = 3;
+let numberTrees = 8;
 let trees = [];
+let trunkPieces = 10;
+let lenInf = 80;
+let lenSup = 100;
+
+let numberStars = 35;
+let stars = [];
+let numberShootingStars = 1;
+let shootingStars = [];
 
 function setGradient(c1, c2) {
   noFill();
@@ -27,11 +30,14 @@ function setGradient(c1, c2) {
 }
 
 function setup() {
+  WIDTH = windowWidth;
+  HEIGHT = windowHeight;
   cnv = createCanvas(WIDTH, HEIGHT);
   let sizeX = (windowWidth-WIDTH)/2;
   let sizeY = (windowHeight-HEIGHT)/2;
   cnv.position(sizeX, sizeY);
   angleMode(DEGREES);
+  frameRate(60);
 
   loadRule();
 
@@ -40,21 +46,45 @@ function setup() {
     trees[i].generate();
   }
 
-  c1 = color(14, 4, 56);
-  c2 = color(95, 6, 97);
+  for (let i = 0; i < numberStars; i++) {
+    stars[i] = new Stars();
+  }
+
+  for (let i = 0; i < numberShootingStars; i++) {
+    shootingStars[i] = new Shooting();
+  }
+
+  c1 = color(40,15,54,255);
+  c2 = color(240,159,156,255);
   setGradient(c1, c2);
 }
 
 function draw() {
   stroke(0);
+  c1 = color(40,15,54,30);
+  c2 = color(240,159,156,30);
+  setGradient(c1, c2);
+
+  for (let i = 0; i < numberStars; i++) {
+    stars[i].update();
+    stars[i].draw();
+  }
+
+  for (let i = 0; i < numberShootingStars; i++) {
+    if (random() < 0.05) {
+      shootingStars[i].revive()
+    }
+    shootingStars[i].update();
+    shootingStars[i].draw();
+  }
 
   for (let i = 0; i < numberTrees; i++) {
     push();
-    translate(WIDTH/2, HEIGHT);
+    stroke(0);
     trees[i].draw();
     pop();
   }
-  noLoop();
+  //noLoop();
 }
 
 function loadRule() {
@@ -72,18 +102,18 @@ function loadRule() {
     // };
 
     lSystem = {
-        "axiom" : "TFZ",
+        "axiom" : "TZ",
         "rules" : {
-        0 : "[+[F[Z]]-[F[Z]]][Z]",
-        1 : "[-[F[Z]]+[F[Z]]][Z]",
-        2 : "+[FZ-Z]-[Z]+[[FZ]]-[FZ]",
-        3 : "-[FZ+Z]+[Z]-[[FZ]]+[FZ]",
-        "F" : "F"
+          0 : "F[+FZ[-FZ]]F[-FZ]",
+          1 : "F[-FZ[+FZ]]F[+FZ]",
+          2 : "[[+F[FZ]][-F[FZ]]]+F[FZ]",
+          3 : "[-[F[FZ]][+F[FZ]]]-F[FZ]",
+          "F" : "F"
         },
         "constants" : {
-        "numberRules" : 2,
-        "[" : push,
-        "]" : pop
+          "numberRules" : 4,
+          "[" : push,
+          "]" : pop
         }
     };
 }
@@ -119,6 +149,9 @@ function applyRule() {
       strokeWeight(lineWeight);
       line(x, y, x, y-scale*len);
       translate(0, -scale*len);
+    } else if (variable == "T") {
+      strokeWeight(lineWeight);
+      drawTrunk();
     } else if (variable == "+") {
       rotate(angle+15*noise(len));
     } else if (variable == "-") {
@@ -135,34 +168,139 @@ function applyRule() {
   }
 }
 
+class Stars {
+  constructor() {
+    this.x = random(WIDTH);
+    this.y = random(20, HEIGHT/3);
+    this.r = random(0.25, 3);
+
+    this.lifeTime = floor(random(42));
+  }
+
+  update() {
+    this.lifeTime += 1;
+    this.r += 0.01*sin(this.lifeTime);
+  }
+
+  draw() {
+    noStroke();
+    fill(255);
+    ellipse(this.x, this.y, this.r, this.r);
+    noFill();
+  }
+}
+
+class Shooting {
+  constructor() {
+    this.fallingSide = random();
+    this.x = random(50, WIDTH-50);
+    this.y = random(20, HEIGHT/3);
+    this.r = random(0.25, 3);
+    this.fallingSpeed = random(0.5, 2);
+
+    this.lifeTime = random(30, 80);
+    this.lifeReduction = 1;
+    this.dead = true;
+  }
+
+  revive() {
+    if (this.dead) {
+      this.fallingSide = random();
+      this.x = random(50, WIDTH-50);
+      this.y = random(20, HEIGHT/3);
+      this.r = random(0.5, 3);
+      this.fallingSpeed = 1;
+
+      this.lifeTime = floor(random(30, 80));
+      this.lifeReduction = 1;
+      this.dead = false;
+    }
+  }
+
+  update() {
+    if (this.fallingSide < 0.5) {
+      this.x -= 2;
+    } else {
+      this.x += 2;
+    }
+    this.y += this.fallingSpeed;
+    this.lifeTime -= this.lifeReduction;
+    if (this.lifeTime <= 0) {
+      this.dead = true;
+    }
+  }
+
+  draw() {
+    if (!this.dead) {
+      noStroke();
+      fill(255);
+      ellipse(this.x, this.y, this.r, this.r);
+      noFill();
+    }
+  }
+}
+
 class Tree {
   constructor() {
     this.sequence = "";
-    this.scale = 0.8;
+    this.scale = random(0.2, 0.3);
+    this.len = random(lenInf, lenSup);
+    this.lw = map(this.len, lenInf, lenSup, 5, 7);
+    this.angle = 35;
+    this.posX = random(WIDTH);
+    this.posY = random(HEIGHT, HEIGHT+50);
+
+    this.windAngle = random(6, 10);
+    this.windSpeed = random(0.004, 0.008);
+    this.rigidity = map(this.len, 10, lenSup, 1.0, 0.2);
+    this.windDiff = this.windAngle*(noise(frameCount*this.windSpeed)-0.05)*this.rigidity;
+
+    this.trunkAngles = [];
   }
 
   generate() {
     this.sequence = doRecursion(numberRecursions);
+    console.log(this.sequence);
+
+    for (let i = 0; i < trunkPieces; i++) {
+      this.trunkAngles[i] = random(-5,5);
+    }
+  }
+
+  drawTrunk() {
+    for (let i = 0; i < trunkPieces; i++) {
+      rotate(this.trunkAngles[i]);
+      line(x, y, x, y-this.len/trunkPieces);
+      translate(0, -this.len/trunkPieces);
+    }
   }
 
   draw() {
+    translate(this.posX, this.posY);
     for (let i = 0; i < this.sequence.length; i++) {
       let variable = this.sequence[i];
       if (variable == "F") {
-        strokeWeight(lineWeight);
-        line(x, y, x, y-this.scale*len);
-        translate(0, -this.scale*len);
+        strokeWeight(this.lw);
+        line(x, y, x, y-this.scale*this.len);
+        translate(0, -this.scale*this.len);
+      } else if (variable == "T") {
+        strokeWeight(this.lw);
+        this.drawTrunk();
       } else if (variable == "+") {
-        rotate(angle+15*noise(len));
+        this.rigidity = map(this.len, 10, lenSup, 1.0, 0.2);
+        this.windDiff = this.windAngle*(noise(frameCount*this.windSpeed)-0.05)*this.rigidity;
+        rotate(this.angle+15*noise(this.len)+this.windDiff);
       } else if (variable == "-") {
-        rotate(-angle+15*noise(len));
+        this.rigidity = map(this.len, 10, lenSup, 1.0, 0.2);
+        this.windDiff = this.windAngle*(noise(frameCount*this.windSpeed)-0.05)*this.rigidity;
+        rotate(-this.angle+15*noise(this.len)+this.windDiff);
       } else if (variable == "[") {
-        len *= 0.85;
-        lineWeight *= 0.85;
+        this.len *= 0.85;
+        this.lw *= 0.8;
         push();
       } else if (variable == "]") {
-        len /= 0.85;
-        lineWeight /= 0.85;
+        this.len /= 0.85;
+        this.lw /= 0.8;
         pop();
       }
     }
